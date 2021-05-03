a931dac53050dbc4f802619baca34fdb5a4cef29
tb-eng <73458383+tb-eng@users.noreply.github.com>
2021-05-03 23:16:06 +0200

    MusicHub

---
+++ project/src/test/java/project/AlbumTest.java
@@ -0,0 +1,9 @@
+
+package musichub.main; import static org.junit.jupiter.api.Assertions.*; import org.junit.jupiter.api.Test; class AlbumTest {​​​​     @Test
+
+    void Albumtest() {​​​​
+
+        Album junit = new Album();
+
+    }​​​​ }​​​​
+
---
+++ project/src/test/java/project/AudioElementTest.java
@@ -0,0 +1,9 @@
+
+package musichub.main; import static org.junit.jupiter.api.Assertions.*; import org.junit.jupiter.api.Test; class AudioElementTest {​​​​​ @Test
+
+void AudioElementtest() {​​​​​
+
+AudioElement junit = new AudioElement();
+
+}​​​​​ }​​​​​
+
